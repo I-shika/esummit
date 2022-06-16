99f3526ffc46d6fffbb5179161c0b59f8867a998
Ishika <ishika@ch.iitr.ac.in>
2022-06-16 23:51:45 +0530

    overview

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import * as fs from 'node:fs';
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+  fs.readFile("data/trial-one.json",'utf-8',(err,data)=>{console.log(req.query)
+  res.status(200).json(JSON.parse(data))
+  })
+} 
